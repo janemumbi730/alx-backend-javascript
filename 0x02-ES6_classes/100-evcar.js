e864b6002ee2d9450a96af098726a51fd6762c3f
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2024-01-04 22:49:51 +0300

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -6,8 +6,8 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   cloneCar() {
-    const Clone = super.constructor[Symbol.species];
-    return new Clone();
+    return new Car();
   }
 }
