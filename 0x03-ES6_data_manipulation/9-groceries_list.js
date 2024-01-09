5d9ebfe04efc0a046a18ac1618b1d4622e917093
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2024-01-09 10:25:15 +0300

    task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const map1 = new Map();
+  map1.set('Apples', 10)
+    .set('Tomatoes', 10)
+    .set('Pasta', 1)
+    .set('Rice', 1)
+    .set('Banana', 5);
+  return map1;
+}
